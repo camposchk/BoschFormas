39e8ab22e07c887454fe2a50bef9c8d07abe1581
Mekafuyu <mayconbrunob@hotmail.com>
2024-04-30 15:11:42 -0300

    talvez

--- public/js/Game.js
+++ public/js/Game.js
@@ -1,11 +1,22 @@
-var inputFinish = document.getElementById("validation");
 const finishAtv = "FINALIZAR";
 const code = window.location.href.split('/').slice(-1)
 console.log(code)
 
 function finish() {
-  if (validation.value == finishAtv) {
-    return true;
+  var inputFinish = $("#validation").val();
+  console.log(inputFinish)
+  if (inputFinish == finishAtv) {
+    $.ajax({
+      url: `http://${url}:3000/final-answer/${code}`,
+      type: "PATCH",
+      data: $("#form-res").serialize(),
+      success: function (response) {
+        window.location.replace(`http://${url}:3000/finished`);
+      },
+      error: function (xhr, status, error) {
+        alert("ocorreu um erro")
+      },
+    });
   }
   return false;
 }
@@ -17,10 +28,9 @@ function updateWeights() {
     type: "PATCH",
     data: $("#form-res").serialize(),
     success: function (response) {
-      console.log('works', response)
     },
     error: function (xhr, status, error) {
-      console.log("Error:", error);
+      alert("ocorreu um erro")
     },
   });
 }
